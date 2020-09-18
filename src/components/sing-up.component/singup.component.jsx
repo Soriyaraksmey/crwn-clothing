f56487ce3399f04f69c116c884d14a9589d1402e
Soriyaraksmey <kohosoriya@gmail.com>
2020-09-18 10:28:39 +0700

    implerment firebase util, include ability to store authuticate user sing up

---
+++ src/components/sing-up.component/singup.component.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import FormInput from "../form-input-component/form-input.component";
+import CustomeButton from "../custom-button/custome-button.component";
+
+import {
+  auth,
+  createUserProfileDocument,
+} from "../firebse-util/firebase.utils";
+
+import "./sign-up.styles.scss";
+
+class Singup extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      displayname: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { displayname, email, password, confirmPassword } = this.state;
+
+    if (password !== confirmPassword) {
+      alert("password dont match");
+      return;
+    }
+
+    try {
+      const user = auth.createUserWithEmailAndPassword(email, password);
+
+      await createUserProfileDocument(user, { displayname });
+
+      this.setState = {
+        displayname: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  handlechange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { displayname, email, password, confirmPassword } = this.state;
+    return (
+      <div className="sing-up">
+        <h2 className="title">I do have an account</h2>
+        <span>Sing up with email and your password</span>
+        <form className="sing-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayname"
+            value={displayname}
+            handelChange={this.handlechange}
+            label="Display Name"
+            required
+          />
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            handelChange={this.handlechange}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            handelChange={this.handlechange}
+            label="Password"
+            required
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            handelChange={this.handlechange}
+            label="Confirm Password"
+            required
+          />
+          <CustomeButton type="submit">SING UP</CustomeButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Singup;
--- src/pages/sing-in-sing-up/sing-in-sing-up.componet.jsx
+++ src/pages/sing-in-sing-up/sing-in-sing-up.componet.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Singup from "../../components/sing-up.component/singup.component";
 import Singin from "../../components/singin/singin.component";
+
+import "./sing-in-singup.componet.scss";
 
 const SingInAndSingupPage = () => {
   return (
-    <div>
+    <div className="sing-in-sing-up">
       <Singin />
+      <Singup />
     </div>
   );
 };
